e2cf1bdde6fdc47081e855233b372be8694a78c5
Mike Joyce <mjoyce91@gmail.com>
2016-07-06 10:07:33 -0400

    update
    test

--- api/models/User.js
+++ api/models/User.js
@@ -8,7 +8,7 @@
 module.exports = {
 
   attributes: {
-
+    name: 'String',
+    human: 'Boolean'
   }
 };
-
--- config/models.js
+++ config/models.js
@@ -11,6 +11,8 @@
 
 module.exports.models = {
 
+  schema: true,
+
   /***************************************************************************
   *                                                                          *
   * Your app's default connection. i.e. the name of one of your app's        *
